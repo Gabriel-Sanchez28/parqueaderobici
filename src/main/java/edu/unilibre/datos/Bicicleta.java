7e3fb94291b4a504e66f5ca2471440d4a9c8dc7b
Gabriel-San <gabriele-sanchezf@unilibre.edu.co>
2026-09-11 17:52:24 -0500

    Creacion datos

--- src/main/java/edu/unilibre/datos/Bicicleta.java
+++ src/main/java/edu/unilibre/datos/Bicicleta.java
@@ -4,4 +4,34 @@ public class Bicicleta {
     private int serial;
     private String color;
     private Propietario idpropietario;
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Propietario getIdpropietario() {
+        return idpropietario;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setIdpropietario(Propietario idpropietario) {
+        this.idpropietario = idpropietario;
+    }
+
+    public Bicicleta (int serial, String color, Propietario idpropietario){
+        this.serial = serial;
+        this.color = color;
+        this.idpropietario = idpropietario;
+    }
 }
--- src/main/java/edu/unilibre/datos/Propietario.java
+++ src/main/java/edu/unilibre/datos/Propietario.java
@@ -3,4 +3,25 @@ package edu.unilibre.datos;
 public class Propietario {
     private int identificacion;
     private String nombre;
+
+    public int recibirIdentificacion() {
+        return identificacion;
+    }
+
+    public String recibirNombre() {
+        return nombre;
+    }
+
+    public void modificarIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void modificarNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Propietario (int identificacion, String nombre){
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+    }
 }
