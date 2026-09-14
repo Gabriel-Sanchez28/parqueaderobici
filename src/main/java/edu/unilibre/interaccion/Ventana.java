afeff72c53972f06493afc9c1678b50ddc32c6c0
Gabriel-San <gabriele-sanchezf@unilibre.edu.co>
2026-09-13 20:43:15 -0500

    ventana funcional terminada

--- src/main/java/edu/unilibre/interaccion/Ventana.java
+++ src/main/java/edu/unilibre/interaccion/Ventana.java
@@ -2,74 +2,94 @@ package edu.unilibre.interaccion;
 import edu.unilibre.datos.Pago;
 import edu.unilibre.datos.Propietario;
 import edu.unilibre.gestion.GestorParqueadero;
-
 import java.util.Scanner;
 
 public class Ventana {
     static void main() {
-        Scanner teclado = new Scanner(System.in);
-        GestorParqueadero gestor = new GestorParqueadero();
         int opcion = 0;
-        boolean validacion = false;
-        System.out.println("-------------------------------------------------");
-        System.out.println("|        Menu Parqueadero de Bicicletas         |");
-        System.out.println("-------------------------------------------------");
-        System.out.println("1. Registrar una bicicleta");
-        System.out.println("2. Sacar bicicleta");
-        System.out.println("3. Generar reporte");
+        int retorno = 0;
+        do {
+            while (retorno == 0) {
+                Scanner teclado = new Scanner(System.in);
+                GestorParqueadero gestor = new GestorParqueadero();
 
-        while (!validacion) {
-            System.out.println("Ingrese una opcion: ");
-            if(teclado.hasNextInt()) {
-                opcion = teclado.nextInt();
-                if (opcion >= 1 && opcion <= 3) {
-                    validacion = true;
+                System.out.println("-------------------------------------------------");
+                System.out.println("|        Menu Parqueadero de Bicicletas         |");
+                System.out.println("-------------------------------------------------");
+                System.out.println("1. Registrar una bicicleta");
+                System.out.println("2. Sacar bicicleta");
+                System.out.println("3. Generar reporte");
+                System.out.println("4. Salir");
+
+                if (teclado.hasNextInt()) {
+                    opcion = teclado.nextInt();
+
                 } else {
-                    System.out.println("¡Opcion no valida!");
-                    System.out.println("");
+                    opcion = 0;
                 }
-            }else {
-                System.out.println("¡Opcion no valida!");
-                System.out.println("");
-                teclado.next();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Ingrese el serial de la bicicleta");
+                        int serial = teclado.nextInt();
+                        teclado.nextLine();
+                        System.out.println("Ingrese el color de la bicicleta");
+                        String color = teclado.nextLine();
+
+                        System.out.println("Ingrese su nombre");
+                        String nombre = teclado.nextLine();
+
+                        System.out.println("Ingrese su numero de identificacion");
+                        int id = teclado.nextInt();
+                        Propietario propietario = new Propietario(id, nombre);
+                        boolean agregar = gestor.adicionarBicicleta(serial, color, propietario.recibirIdentificacion());
+                        System.out.println("");
+                        System.out.println("!Bicicleta registrada exitosamente¡");
+                        for (int i = 5; i>=0; i--){
+                            // \r vuelve al inicio de la línea y sobreescribe lo que estaba antes
+                            System.out.print("\rVolviendo al menu inicial en " + i);
+                            System.out.flush();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                        System.out.println("");
+                        break;
+                    case 2:
+                        System.out.println("Ingrese su numero de identificacion");
+                        int idRegistrada = teclado.nextInt();
+                        teclado.nextLine();
+                        gestor.metodoPago();
+                        System.out.println("Ingrese un metodo de pago tal cual como esta escrito: ");
+                        String pago = teclado.nextLine();
+                        Pago pagar = null;
+                        try {
+                            pagar = Pago.valueOf(pago);
+                        } catch (IllegalArgumentException e) {
+                            boolean sacar = gestor.registrarSalida(idRegistrada, null);
+                        }
+
+                        boolean sacar = gestor.registrarSalida(idRegistrada, pagar);
+                        if (sacar == false) {
+                            System.out.println("Ocurrio un error, verifica los datos ingresados");
+                        }
+                        break;
+                    case 3:
+                        String reporte = gestor.generarReporte();
+                        System.out.println(reporte);
+                        break;
+                    case 4:
+                        System.out.println("Gracias por usar nuestros servicios");
+                        retorno = 1;
+                        break;
+                    default:
+                        System.out.println("¡Opcion no valida1!");
+                        System.out.println("");
+                }
             }
-
-        }
-        switch (opcion){
-            case 1:
-                System.out.println("Ingrese el serial de la bicicleta");
-                int serial = teclado.nextInt();
-                teclado.nextLine();
-                System.out.println("Ingrese el color de la bicicleta");
-                String color = teclado.nextLine();
-
-                System.out.println("Ingrese su nombre");
-                String nombre = teclado.nextLine();
-
-                System.out.println("Ingrese su numero de identificacion");
-                int id = teclado.nextInt();
-                Propietario propietario = new Propietario(id, nombre);
-                boolean agregar = gestor.adicionarBicicleta(serial, color, propietario.recibirIdentificacion());
-                System.out.println("!Bicicleta registrada exitosamente¡");
-                break;
-            case 2:
-                System.out.println("Ingrese su numero de identificacion");
-                int idRegistrada = teclado.nextInt();
-                teclado.nextLine();
-                gestor.metodoPago();
-                System.out.println("Ingrese un metodo de pago tal cual como esta escrito: ");
-                String pago = teclado.nextLine();
-                Pago pagar = Pago.valueOf(pago);
-
-                boolean sacar = gestor.registrarSalida(idRegistrada,pagar);
-                if (sacar == false){
-                    System.out.println("No se encontro ninguna bicicleta registrada, verifica el documento ingresado");
-                }
-                break;
-            case 3:
-                String reporte= gestor.generarReporte();
-                System.out.println(reporte);
-                break;
-        }
+        }while (opcion!=4);
     }
 }
